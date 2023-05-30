d4a1cc6dc4b4af36d9f92a5d60ba2bf843983e9f
metalxperts <metalxperts.aws@gmail.com>
2023-05-30 12:58:27 +0530

    update

--- project_mx/urls.py
+++ project_mx/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("app_django.urls")),
+    path('', include("app_mx_website.urls")),
 ]
 
